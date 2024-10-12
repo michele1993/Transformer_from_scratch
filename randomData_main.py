f638a61e30ae747f14c21c965c48854fac2f9af2
Michele Garibbo <michele.garibbo@bristol.ac.uk>
2024-10-12 18:36:16 +0100

    Define tokenization and numeralization functions

--- randomData_main.py
+++ randomData_main.py
@@ -1,19 +1,18 @@
 import torch
 from transformer import Transformer
 from utils import setup_logger
+from utils import generate_randomData_batch
 import logging
 
 
 # Initialise useful variables
 tot_ep = 100 # n. of training step
 vocab_size = 50 # total n. of token
-batch_s = 50 # batch size
-input_seq_s = 10 # length of the input sequence
-target_seq_s = 12 # length of the ouput sequence
 max_seq_len = 20 # max sequence length (for positional encoding)
 embedding_s= 512 # size of the embedding space
 n_heads= 4 # n. of attention heads
 ln_rate = 5e-4 # learning rate
+
 
 # Select correct device
 if torch.cuda.is_available():
@@ -24,26 +23,12 @@ else:
     dev='cpu'
 
 
-assert target_seq_s <= max_seq_len and input_seq_s <= max_seq_len, "Sequence size larger than max sequence size"
+# Generate some artificial data
+x, target_x, start_token, end_token = generate_randomData_batch(max_seq_len=max_seq_len, dev=dev, vocab_size=vocab_size)
 
 # set-up logger
 setup_logger()
 
-# Generate random input and target sequences by initialising a list of indexes for each batch 
-#e.g., each index could denote a different word, where the input sequence is words in english while the target sequence is the translation in italian.
-x = torch.randint(1,vocab_size-2,(batch_s,input_seq_s)).to(dev)
-target_x = torch.randint(1,vocab_size-2,(batch_s,target_seq_s)).to(dev)
-
-# --- To test inference performance and ability to stop, I use vocab_size-1 and vocab_size-2 as the  start and end token respectively
-start_token = vocab_size-2
-end_token = vocab_size -1
-
-# Add start token at beginning of sequence 
-x[:,0] = start_token
-target_x[:,0] = start_token
-# Add end token at end of sequence 
-x[:,-1] = end_token
-target_x[:,-1] = end_token
 
 # Initialise overall stransformer architecture
 transformer = Transformer(input_vocab_s=vocab_size, target_vocab_s=vocab_size, max_seq_len=max_seq_len, embedding_s=embedding_s, n_heads=n_heads, ln_rate=ln_rate).to(dev)
--- utils.py
+++ utils.py
@@ -8,3 +8,26 @@ def setup_logger(seed=None):
     if seed is not None:
         logging.info(f'Seed: {seed}')
 
+def generate_randomData_batch(max_seq_len, dev, batch_s=50, input_seq_s=10, target_seq_s=12, vocab_size=50):
+
+
+    assert target_seq_s <= max_seq_len and input_seq_s <= max_seq_len, "Sequence size larger than max sequence size"
+
+    # Generate random input and target sequences by initialising a list of indexes for each batch 
+    #e.g., each index could denote a different word, where the input sequence is words in english while the target sequence is the translation in italian.
+    x = torch.randint(1,vocab_size-2,(batch_s,input_seq_s)).to(dev)
+    target_x = torch.randint(1,vocab_size-2,(batch_s,target_seq_s)).to(dev)
+
+    # --- To test inference performance and ability to stop, I use vocab_size-1 and vocab_size-2 as the  start and end token respectively
+    start_token = vocab_size-2
+    end_token = vocab_size -1
+
+    # Add start token at beginning of sequence 
+    x[:,0] = start_token
+    target_x[:,0] = start_token
+    # Add end token at end of sequence 
+    x[:,-1] = end_token
+    target_x[:,-1] = end_token
+
+    return x, target_x, start_token, end_token
+
